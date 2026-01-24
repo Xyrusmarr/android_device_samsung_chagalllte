ff1319d15eb667673cb3343c3a5d107194704183
Xyrusmarr <127576138+Xyrusmarr@users.noreply.github.com>
2026-01-24 15:15:33 -0500

    Update device model and build properties for chagalllte

--- lineage_chagalllte.mk
+++ lineage_chagalllte.mk
@@ -5,22 +5,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/samsung/chagalllte/device.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit more LineageOS stuff.
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/lineage/config/telephony.mk)
 
 PRODUCT_NAME := lineage_chagalllte
 PRODUCT_DEVICE := chagalllte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SM-T805
+PRODUCT_MODEL := SM-T805W
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_MODEL=SM-T805 \
+    PRODUCT_MODEL=SM-T805W \
     PRODUCT_NAME=chagalllte \
     PRODUCT_DEVICE=chagalllte \
-    PRIVATE_BUILD_DESC="samsung/chagallltezh/chagalllte:6.0.1/MMB29K/T805ZHU1CQB1:user/release-keys" \
+    PRIVATE_BUILD_DESC="samsung/chagallltezh/chagalllte:6.0.1/MMB29K/T805WVLU1BVG1:user/release-keys" \
     TARGET_DEVICE=chagalllte
 
-BUILD_FINGERPRINT := samsung/chagallltezh/chagalllte:6.0.1/MMB29K/T805ZHU1CQB1:user/release-keys
+BUILD_FINGERPRINT := samsung/chagallltezh/chagalllte:6.0.1/MMB29K/T805WVLU1BVG1:user/release-keys
