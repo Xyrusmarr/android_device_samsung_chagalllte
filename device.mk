8de5f3d5bf015feb05abf47dc8687c06ccd004d1
Xyrusmarr <127576138+Xyrusmarr@users.noreply.github.com>
2026-01-24 18:48:05 -0500

    Update product inheritance calls in device.mk

--- device.mk
+++ device.mk
@@ -83,12 +83,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown
 
 # call dalvik heap and hwui config
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/chagalllte/chagalllte-vendor.mk)
-
-# Import the common tree changes
-include device/samsung/universal5420-common/universal5420.mk
