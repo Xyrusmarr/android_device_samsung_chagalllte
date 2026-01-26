dcefd60221a0447daab8e6030144ca866a1f2503
Xyrusmarr <127576138+Xyrusmarr@users.noreply.github.com>
2026-01-25 19:50:08 -0500

    Remove 'eng' tag from ueventd module

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -26,7 +26,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional 
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= ueventd.universal5420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
